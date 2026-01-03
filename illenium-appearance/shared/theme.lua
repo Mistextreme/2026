4dcbdfd982dad05dec496788a543951427483c15
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2026-01-03 17:52:36 +0000

    Upload: illenium-appearance/shared/theme.lua

---
+++ illenium-appearance/shared/theme.lua
@@ -0,0 +1,53 @@
+-- Make sure to follow the required format on colors for each section.
+return {
+    -- Colors (HSL)
+    ["background"] = "0 0% 6.7%",
+    ["background-alt"] = "0 0% 11%",
+    ["foreground"] = "0 0% 94.9%",
+    ["foreground-alt"] = "0 0% 70.2%",
+    ["muted"] = "240 5.9% 16.7%",
+    ["muted-foreground"] = "0 0% 100% / 0.4",
+    ["dark"] = "0 0% 96.1%",
+    ["dark-04"] = "0 0% 96% / 0.4",
+    ["dark-10"] = "0 0% 96% / 0.1",
+    ["dark-40"] = "0 0% 96% / 0.04",
+    ["accent"] = "201 88% 87%",
+    ["destructive"] = "347 78% 60%",
+    ["border"] = "0 0% 16.5%",
+    ["border-alt"] = "0 0% 8.2%",
+
+    -- Colors (HEX)
+    ["text"] = "#dadada",
+    ["text-alt"] = "#dadada95",
+    ["icon"] = "#ffffff",
+
+    -- Background Noise Config
+    ["noise-deg"] = "0deg",
+    ["noise-from-color"] = "#FFFFFF1A",
+    ["noise-from-percentage"] = "0%",
+    ["noise-to-color"] = "#FFFFFF1A",
+    ["noise-to-percentage"] = "100%",
+
+    --Gradient Values
+    ["menu-gradient"] = "#111111",
+    ["header-radial-gradient"] = "#262626, #0d0d0f",
+
+    --Shadows
+    ["shadow-mini"] = "0 0.25vmin 0 0.25vmin #0000004D",
+    ["shadow-popover"] = "0 1.75vmin 3vmin 0.75vmin #0000004D",
+
+    -- Border Radius
+    ["none"] = "0",
+    ["sm"] = "0.125vmin",
+    ["DEFAULT"] = "0.25vmin",
+    ["md"] = "0.675vmin",
+    ["lg"] = "1vmin",
+    ["xl"] = "1.55vmin",
+    ["2xl"] = "2vmin",
+    ["3xl"] = "2.5vmin",
+    ["card"] = "1.6vmin",
+    ["card-sm"] = "1.0vmin",
+    ["card-lg"] = "2.0vmin",
+    ["input"] = "0.9vmin",
+    ["button"] = "1vmin",
+}
