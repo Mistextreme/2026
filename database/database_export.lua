c888b4e0c9bdb230b8a7505ae595e9c8652543ec
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2026-01-03 16:42:05 +0000

    Upload: database_export.lua

---
+++ database/database_export.lua
@@ -0,0 +1,36 @@
+-- Complete Database Export
+
+local database = {}
+
+-- Pictures
+database.pictures = -- FiveManage Images
+{
+    ["file_00000000ad58720a9b44567b4263efaf"] = {
+        label = "file_00000000ad58720a9b44567b4263efaf",
+        image = "https://raw.githubusercontent.com/Mistextreme/2026/main/file_00000000ad58720a9b44567b4263efaf.png?token=AO2YHHGRI7LHMVLVZU6SBMLJLFDGDAA"
+    }
+}
+
+-- Videos
+database.videos = -- FiveManage Videos
+{
+    ["Code_intro"] = {
+        label = "Code_intro",
+        video = "https://raw.githubusercontent.com/Mistextreme/2026/main/Code_intro.mp4?token=AO2YHHH4ITX7LPJNDIQ33KDJLFDGDAA"
+    },
+    ["mist_intro"] = {
+        label = "mist_intro",
+        video = "https://raw.githubusercontent.com/Mistextreme/2026/main/mist_intro.mp4?token=AO2YHHGD7HE5ZMQJ4OTV4LTJLFDGDAA"
+    }
+}
+
+-- Gifs
+database.gifs = -- FiveManage Gifs
+{
+    ["mist-intro"] = {
+        label = "mist-intro",
+        image = "https://raw.githubusercontent.com/Mistextreme/2026/main/mist-intro.gif?token=AO2YHHGHM3YOAXJJNTMWVN3JLFDGDAA"
+    }
+}
+
+return database
