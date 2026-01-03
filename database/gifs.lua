93410abbeb14476263595b41a050df49da5806c7
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2026-01-03 16:39:20 +0000

    Update gifs.lua - Added 1 new entries

--- database/gifs.lua
+++ database/gifs.lua
@@ -1,3 +1,7 @@
 -- FiveManage Gifs
 return {
+    ["mist-intro"] = {
+        label = "mist-intro",
+        image = "https://raw.githubusercontent.com/Mistextreme/2026/main/mist-intro.gif?token=AO2YHHGHM3YOAXJJNTMWVN3JLFDGDAA"
+    }
 }
