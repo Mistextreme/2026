f62de0db20383b3311579f08c8a8a996a61eef4d
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2026-01-03 17:49:35 +0000

    Upload: rhud/fxmanifest.lua

---
+++ rhud/fxmanifest.lua
@@ -0,0 +1,36 @@
+--Decrypted by FXAP-Decrypter 👿 | discord.gg/fxapdecrypter
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Raxdiam'
+description 'Standalone roleplay HUD'
+version '1.7.1'
+
+lua54 'yes'
+
+ui_page 'ui/index.html'
+
+files {
+  'ui/sounds/*.ogg',
+  'ui/icon.png',
+  'ui/index.html',
+  'ui/icons/*.png',
+  'locale/*.json',
+  'data/*.json',
+}
+
+shared_scripts {
+  'config.lua',
+  'script/shared.lua',
+}
+client_scripts {
+  'functions.lua',
+  'script/client.lua',
+}
+server_script 'script/server.lua'
+
+postal_file 'data/postals.json'
+
+dependencies {
+  '/assetpacks',
+}
